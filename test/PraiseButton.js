6a0932bed5eb1a8f5bfc267a5deee6abfdb8502c
wy <721777936@qq.com>
2018-04-15 23:08:01 +0800

    单元测试

---
+++ test/PraiseButton.js
@@ -0,0 +1,13 @@
+class PraiseButton {
+    constructor(initCount = 0) {
+        this.PraiseCount = initCount;
+    }
+
+    addCount() {
+        this.PraiseCount += 1;
+        this.changeCount(this.PraiseCount);
+    }
+
+    changeCount() {}
+
+}
